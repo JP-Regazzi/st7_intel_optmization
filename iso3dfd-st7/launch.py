2aa63054e930f1a00a74f17d9ede5710f9fc2dbf
benjamin-feldman <benjamin.feldman666@gmail.com>
2024-03-01 16:00:44 +0100

    structure

--- iso3dfd-st7/launch.py
+++ iso3dfd-st7/launch.py
@@ -12,21 +12,23 @@ def extract_fitness(text):
     fitness = re.search(pattern, text).group()
     return fitness
 
-def deploySUBP(cache_blocking, n_items=100, n_threads=32):
+def deploySUBP(cache_blocking, n_items=100, n_threads=32, verbose=0):
     """
     :param int n_threads
     :param int n_items
     :param (int*int*int) cache_blocking
     """
     msg = f"Parameters: \n\t- Number of threads: {n_threads} \n\t- Number of items: {n_items} \n\t- Cache blocking: {cache_blocking}"
-    print(msg)
+
     cmd = f"bin/iso3dfd_dev13_cpu_avx2.exe {NB_OF_PB} {NB_OF_PB} {NB_OF_PB} {n_threads} {n_items} {cache_blocking[0]} {cache_blocking[1]} {cache_blocking[2]}"
-    print(f"Executed command: {cmd}")
-    print("**********")
     res = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE)
     output = str(res.stdout,'utf-8')
     fitness = extract_fitness(output)
-    print(f"Fitness:{fitness}")
+    if verbose:
+        print(f"Executed command: {cmd}")
+        print(msg)
+        print(f"Fitness: {fitness}")
+    return float(fitness)
 
 def cmdLineParsing():
     parser = argparse.ArgumentParser()
@@ -39,6 +41,10 @@ def cmdLineParsing():
     cache_blocking = tuple(args.cache)
     return (n_items, cache_blocking, n_threads)
 
-print("Deployment")
-args = cmdLineParsing()
-deploySUBP(*args)
+def getFitness(c1, c2, c3):
+    return deploySUBP((c1,c2,c3))
+
+if __name__ == "__main__":
+    print("Deployment")
+    args = cmdLineParsing()
+    deploySUBP(*args)
