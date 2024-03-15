11455f8a2055a259be45bf46fc9f1cfcc37674b1
JP-Regazzi <jpregazzi@gmail.com>
2024-03-15 16:19:14 +0100

    Bug fix + Added number of runs counter

--- iso3dfd-st7/grid_search.py
+++ iso3dfd-st7/grid_search.py
@@ -4,28 +4,21 @@ import starter
 
 
 def parallel_grid_search(comm, grid_values_i, grid_values_j, grid_values_k):
+    # Get MPI node attributes
     rank = comm.Get_rank()
     size = comm.Get_size()
 
-    # Divide the grid values among MPI processes
-    part_size_i = len(grid_values_i) // size
-    part_size_j = len(grid_values_j) // size
-    part_size_k = len(grid_values_k) // size
+    # Divide the grid among MPI processes
+    start_i = rank * (len(grid_values_i) // size)
+    end_i = (rank + 1) * (len(grid_values_i) // size)
 
-    start_i = rank * part_size_i
-    end_i = (rank + 1) * part_size_i
-    start_j = rank * part_size_j
-    end_j = (rank + 1) * part_size_j
-    start_k = rank * part_size_k
-    end_k = (rank + 1) * part_size_k
-
-    best_points_local = (1, 1, 1)
-    current_maximum_local = 9999999
+    # Define starting variables
+    current_maximum_local, best_points_local = 0, (0, 0, 0)
 
     # Iterate through the local part of the grid
     for i in grid_values_i[start_i:end_i]:
-        for j in grid_values_j[start_j:end_j]:
-            for k in grid_values_k[start_k:end_k]:
+        for j in grid_values_j:
+            for k in grid_values_k:
                 temp_result = starter.run_process_parametrized([i, j, k])
                 if temp_result > current_maximum_local:
                     current_maximum_local = temp_result
@@ -38,16 +31,16 @@ def parallel_grid_search(comm, grid_values_i, grid_values_j, grid_values_k):
     # Rank 0 prints the global best points
     if rank == 0:
         global_best_index = np.argmax(current_maximum_global)
-        print("Best Points (Global):", best_points_global[global_best_index])
-        print("Current Maximum (Global):", current_maximum_global[global_best_index])
+        print("Best Points:", best_points_global[global_best_index])
+        print("Current Maximum:", current_maximum_global[global_best_index])
+        print("Total number of runs:", len(grid_values_i)*len(grid_values_j)*len(grid_values_k))
 
 
-if __name__ == "__main__":
-    comm = MPI.COMM_WORLD
-    rank = comm.Get_rank()
 
-    grid_values_i = range(32, 256, 32)
-    grid_values_j = range(32, 256, 32)
-    grid_values_k = range(32, 256, 32)
+comm = MPI.COMM_WORLD
 
-    parallel_grid_search(comm, grid_values_i, grid_values_j, grid_values_k)
+grid_values_i = range(32, 256, 32)
+grid_values_j = range(32, 256, 32)
+grid_values_k = range(32, 256, 32)
+
+parallel_grid_search(comm, grid_values_i, grid_values_j, grid_values_k)
