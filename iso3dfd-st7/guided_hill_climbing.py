8235b0cd6b2dcc4fa0a3d67efd050ac6ef0bea86
JP-Regazzi <jpregazzi@gmail.com>
2024-03-15 15:41:23 +0100

    Added number of runs counter

--- iso3dfd-st7/guided_hill_climbing.py
+++ iso3dfd-st7/guided_hill_climbing.py
@@ -12,17 +12,20 @@ def update_parameters(parameters, step_size):
     return parameters
 
 
-def hill_climbing(initial_parameters, step_size, max_stable_runs):
-    current_parameters = initial_parameters
-    print(initial_parameters)
-    current_gflops = run_process_parametrized(current_parameters[0], current_parameters[1], current_parameters[2])
+def guided_hill_climbing(initial_parameters, step_size, max_stable_runs):
+
+    number_of_runs = 0
     stable_runs = 0
+
+    current_parameters = initial_parameters
+    current_gflops, number_of_runs = run_process_parametrized(current_parameters),  number_of_runs + 1
     
-    #Implement guided hill climbing
+    # Guided hill climbing
     covered_zones = []
     begin_zone = None
     value_zone = 0
     init_zone = True
+    
     while True:
 
         # Update parameters
@@ -33,7 +36,9 @@ def hill_climbing(initial_parameters, step_size, max_stable_runs):
             begin = zone[0][0]
             end = zone[0][1]
             value = zone[1]
-            if neighbor_parameters[0] >= begin[0] and neighbor_parameters[0] <= end[0] and neighbor_parameters[1] >= begin[1] and neighbor_parameters[1] <= end[1] and neighbor_parameters[2] >= begin[2] and neighbor_parameters[2] <= end[2]:
+            if (neighbor_parameters[0] >= begin[0] and neighbor_parameters[0] <= end[0] and
+                neighbor_parameters[1] >= begin[1] and neighbor_parameters[1] <= end[1] and
+                neighbor_parameters[2] >= begin[2] and neighbor_parameters[2] <= end[2]):
                 neighbor_gflops = value
             else:
                 covered_area = True
@@ -41,8 +46,8 @@ def hill_climbing(initial_parameters, step_size, max_stable_runs):
         if covered_area:
             neighbor_gflops = value
         else:
-            neighbor_gflops = run_process_parametrized(neighbor_parameters[0], neighbor_parameters[1], neighbor_parameters[2])
-
+            neighbor_gflops, number_of_runs = run_process_parametrized(neighbor_parameters), number_of_runs + 1
+            
         if neighbor_gflops > current_gflops:
             if init_zone:
                 begin_zone = current_parameters
@@ -63,7 +68,7 @@ def hill_climbing(initial_parameters, step_size, max_stable_runs):
         if stable_runs >= max_stable_runs:
             break
 
-    return current_parameters, current_gflops
+    return current_parameters, current_gflops, number_of_runs
 
 
 def generate_starting_points(num_starting_points):
@@ -77,40 +82,40 @@ def generate_starting_points(num_starting_points):
     return starting_points
 
 
-def main(num_starting_points, max_stable_runs, step_size):
+def main(max_stable_runs, step_size):
     comm = MPI.COMM_WORLD
     rank = comm.Get_rank()
     size = comm.Get_size()
 
     # Split the starting points among processes
+    num_starting_points = size
     starting_points = generate_starting_points(num_starting_points)
     chunk_size = len(starting_points) // size
     starting_points_chunk = starting_points[rank * chunk_size: (rank + 1) * chunk_size]
 
     # Run hill climbing for the assigned starting points
-    local_results = []
+    local_results, number_of_local_runs = [], 0
     for start_point in starting_points_chunk:
-        print(f"starting point: {start_point}")
-        local_results.append(hill_climbing(start_point, step_size, max_stable_runs))
+        params, gflops, number_of_runs = guided_hill_climbing(start_point, step_size, max_stable_runs)
+        local_results.append((params, gflops))
+        number_of_local_runs += number_of_runs
 
     # Gather results from all processes
     all_results = comm.gather(local_results, root=0)
+    total_number_of_runs = comm.reduce(number_of_local_runs, op=MPI.SUM, root=0)
 
     if rank == 0:
         # Flatten the gathered results
         flattened_results = [result for sublist in all_results for result in sublist]
         best_solution = max(flattened_results, key=lambda x: x[1])
         print("Best Global Solution:", best_solution[0], best_solution[1])
+        print("Total number runs:", total_number_of_runs)
         return best_solution
 
     return None
 
-print("teste")
-#if __name__ == "main":
-print('Begin code')    
 # Parameters
-num_starting_points = 8
 max_stable_runs = 20
 step_size = 4
 
-main(num_starting_points, max_stable_runs, step_size)
+main(max_stable_runs, step_size)
